64121eadf471f99fc745fca4bc6b2ad15b2ee22d
arenasesteban <esteban.arenas.a@usach.cl>
2025-02-09 20:40:06 -0300

    [Esteban] Add exercises 01 to 06 for Part 3

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const app = express();
+
+let phoneBook = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+];
+
+app.use(express.json());
+
+app.get('/api/persons', (req, res) => {
+    res.json(phoneBook);
+});
+
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${phoneBook.length} people</p>
+              <p>${new Date()}</p>`);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number (req.params.id);
+    const person = phoneBook.find(person => person.id === id);
+
+    if(person) {
+        res.json(person)
+    } else {
+        res.status(404).end();
+    };
+});
+
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    phoneBook = phoneBook.filter(person => person.id !== id);
+
+    res.status(204).end();  
+});
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+
+    if(!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'name or number missing'
+        });
+    };
+
+    if(phoneBook.find(person => person.name === body.name)) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        });
+    }; 
+
+    const person = {
+        id: Math.floor(Math.random() * 1000),
+        name: body.name,
+        number: body.number
+    };
+
+    phoneBook = [...phoneBook, person];
+
+    res.json(person);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
