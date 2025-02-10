1f42f34318870020b172fc4565f489a98d40109a
arenasesteban <esteban.arenas.a@usach.cl>
2025-02-09 22:41:43 -0300

    [Esteban] Add exercises 07 to 08 for Part 3

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 let phoneBook = [
@@ -25,6 +26,10 @@ let phoneBook = [
 ];
 
 app.use(express.json());
+
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 app.get('/api/persons', (req, res) => {
     res.json(phoneBook);
