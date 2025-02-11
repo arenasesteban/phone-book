996179edb4367f62a01c49c76453b078e14d2951
Esteban Arenas <esteban.arenas.a@usach.cl>
2025-02-11 12:42:46 -0300

    [Esteban] Add exercises 09 to 11 for Part 3

--- frontend/src/components/Filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({ search, setSearch }) => {
-    return (
-        <div>
-            Filter shown with: <input value={search} onChange={e => setSearch(e.target.value)} /> 
-        </div>
-    );
-}
-
-export default Filter;
