b13f763a57cc5af064088d1f8ecdc021c22014ab
arenasesteban <esteban.arenas.a@usach.cl>
2025-02-16 22:50:34 -0300

    [Esteban] Add exercise 12 for Part 3

--- backend/mongo.js
+++ backend/mongo.js
@@ -7,7 +7,7 @@ if(process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://arenasesteban:${password}@arenasesteban-cluster.w9r5d.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://arenasesteban:${password}@cluster0.w9r5d.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.set('strictQuery', false);
 
@@ -20,6 +20,7 @@ mongoose.connect(url).then(() => {
     const Person = mongoose.model('Person', personSchema);
 
     if (process.argv.length === 3) {
+        console.log('Phonebook:');
         Person.find({}).then(result => {
             result.forEach(person => {
                 console.log(`${person.name} ${person.number}`);
@@ -29,7 +30,7 @@ mongoose.connect(url).then(() => {
         }).catch(error => {
             console.log('Error fetching persons:', error);
         });
-    } else if (process.argv === 5) {
+    } else if (process.argv.length === 5) {
         const person = new Person({
             name: process.argv[3],
             number: process.argv[4]
@@ -46,40 +47,3 @@ mongoose.connect(url).then(() => {
 }).catch(error => {
     console.log('Error connecting to MongoDB:', error);
 });
-
-/* 
-const main = async () => {
-    try {
-        await mongoose.connect(url);
-
-        const personSchema = new mongoose.Schema({
-            name: String,
-            number: String
-        });
-
-        const Person = mongoose.model('Person', personSchema);
-
-        if (process.argv.length === 3) {
-            const persons = await Person.find({});
-
-            console.log('Phonebook:');
-            persons.forEach(person => {
-                console.log(`${person.name} ${person.number}`)
-            });
-        } else if (process.argv.length === 5) {
-            const newPerson = new Person({
-                name: process.argv[4],
-                number: process.argv[5]
-            });
-    
-            await newPerson.save();
-            console.log(`Added ${newPerson.name} number ${newPerson.number} to phonebook`);
-        }
-
-        mongoose.connection.close();
-    } catch(error) {
-        console.log('Error connecting to MongoDB:', error);
-    }
-}
-
-main(); */
